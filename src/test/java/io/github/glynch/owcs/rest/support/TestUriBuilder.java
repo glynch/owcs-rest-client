879afc32fd8388606c05d6fbea6fa5b116d7d1b5
Graham Lynch <graham.lynch@gmail.com>
2024-10-22 17:58:01 +0700

    UriBuilder test cases

--- src/test/java/io/github/glynch/owcs/rest/support/TestUriBuilder.java
+++ src/test/java/io/github/glynch/owcs/rest/support/TestUriBuilder.java
@@ -1,6 +1,5 @@
 package io.github.glynch.owcs.rest.support;
 
-import static io.github.glynch.owcs.rest.client.authenticated.AuthenticatedRestClient.TYPE_URI_TEMPLATE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URI;
@@ -11,10 +10,40 @@ import org.junit.jupiter.api.Test;
 public class TestUriBuilder {
 
     @Test
-    void testUriBuilderFromMapSingleVariable() {
-        UriBuilder uriBuilder = new DefaultUriBuilder(TYPE_URI_TEMPLATE);
+    void testFromMapSingleVariable() {
+        UriBuilder uriBuilder = new DefaultUriBuilder("/REST/types/{type}");
         URI uri = uriBuilder.build(Map.of("type", "AVIArticle"));
         assertEquals("/REST/types/AVIArticle", uri.toString());
+    }
+
+    @Test
+    void testFromMapMultipleVariables() {
+        UriBuilder uriBuilder = new DefaultUriBuilder("/REST/sites/{site}/types/{type}/assets/{id}");
+        URI uri = uriBuilder.build(Map.of("site", "avisports", "type", "AVIArticle", "id", 1395380847207L));
+        assertEquals("/REST/sites/avisports/types/AVIArticle/assets/1395380847207", uri.toString());
+    }
+
+    @Test
+    void testFromVarargsSingleVariable() {
+        UriBuilder uriBuilder = new DefaultUriBuilder("/REST/types/{type}");
+        URI uri = uriBuilder.build("AVIArticle");
+        assertEquals("/REST/types/AVIArticle", uri.toString());
+    }
+
+    @Test
+    void testFromVarargsMultipleVariables() {
+        UriBuilder uriBuilder = new DefaultUriBuilder("/REST/sites/{site}/types/{type}/assets/{id}");
+        URI uri = uriBuilder.build("avisports", "AVIArticle", 1395380847207L);
+        assertEquals("/REST/sites/avisports/types/AVIArticle/assets/1395380847207", uri.toString());
+    }
+
+    @Test
+    void testEncodedVariables() {
+        UriBuilder uriBuilder = new DefaultUriBuilder(
+                "/REST/resources/v1/aggregates/{site}/engage/recommendation/{name}/items");
+        URI uri = uriBuilder.encode().build(Map.of("site", "avisports", "name", "Running Recommendation"));
+        assertEquals("/REST/resources/v1/aggregates/avisports/engage/recommendation/Running%20Recommendation/items",
+                uri.toString());
     }
 
 }
