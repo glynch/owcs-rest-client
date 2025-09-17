a9ee880c02293f8bbec7b2f0cb4f5dd5e3595c0d
Graham Lynch <graham.lynch@gmail.com>
2025-09-18 02:06:11 +0700

    Added more IT tests

--- src/test/java/io/github/glynch/owcs/rest/it/TestTypesIT.java
+++ src/test/java/io/github/glynch/owcs/rest/it/TestTypesIT.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import com.fatwire.rest.beans.AssetTypesBean;
+import com.fatwire.rest.beans.Type;
 
 import io.github.glynch.owcs.rest.client.authenticated.AuthenticatedRestClient;
 import io.github.glynch.owcs.test.containers.JSKContainer;
@@ -35,6 +36,18 @@ public class TestTypesIT {
         assertEquals(57, assetTypesBean.getTypes().size());
         assertEquals("AVIArticle", assetTypesBean.getTypes().get(0).getName());
         assertEquals(3, assetTypesBean.getTypes().get(0).getSubtypes().size());
+    }
+
+    @Test
+    void testSubtypes() {
+        AssetTypesBean subtypes = restClient.type("AVIArticle").subtypes();
+        assertEquals(3, subtypes.getTypes().size());
+        Type article = subtypes.getTypes().get(0);
+        assertEquals("Article", article.getName());
+        Type articleImage = subtypes.getTypes().get(1);
+        assertEquals("ArticleImage", articleImage.getName());
+        Type image = subtypes.getTypes().get(2);
+        assertEquals("Image", image.getName());
     }
 
     @AfterAll
